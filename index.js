f344708238c7ebd7103bb40b42a77c3f7074d08f
Lucas Carvalho <maracaipelucas@gmail.com>
2022-09-01 19:15:25 -0300

    fix limit tries

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Declarando as variáveis
 var senha = "";
 var quantidadeDeTentativas = 0;
+var limitarTentativas = true;
+
 
 // Instanciando o módulo readline do Node.js para entrada e saída de dados
 const readline = require('readline').createInterface({
@@ -9,30 +11,58 @@ const readline = require('readline').createInterface({
 });
 
 // Lendo o valor da senha e guardando na variável
-readline.question(`Digite sua senha\n`, senhaInput => {
+readline.question(`Digite sua senha, ela deve conter no mínimo 6 dígitos. Ex.: 123456\n`, senhaInput => {
   senha = senhaInput;
-  console.log("Iniciando força bruta\n");
-  ForcaBruta();
+  if (ValidarInput(senhaInput)) {
+    console.log("Senha inválida");
+  } else {
+    console.log("Iniciando força bruta\n");
+    ForcaBruta();
+  }
 
   readline.close();
 });
 
+// Validar Input
+function ValidarInput(input) {
+  let inputNumber = parseInt(input);
+  let inputLength = inputNumber.length;
+
+  if (isNaN(inputNumber)) {
+    console.log("A senha deve ser numérica");
+    return true;
+  }
+
+  if (inputLength <= 5) {
+    console.log("A senha não pode ter menos de 6 dígitos");
+    return true;
+  }
+  if (inputNumber < 1) {
+    console.log("A senha não pode ser 0");
+    return true;
+  }
+
+  return false;
+}
+
 // Função responsável por fazer a validação da senha informada com as tentativas realizadas pelo algoritmo de força bruta
 function ValidarSenha(senhaInput) {
-  
+
   // Caso o algoritmo consiga quebrar a senha
   if (senhaInput == senha) {
     console.log("Acesso permitido");
     return true;
   } else {
-    
-    // Caso não consiga quebrar
-    console.log("Acesso negado\n");
-    if (quantidadeDeTentativas == 3) {
+    // Caso a contramedida de limite de tentativas seja ativada
+    if (limitarTentativas) {
+      // Caso não consiga quebrar
+      console.log("Acesso negado\n");
+      if (quantidadeDeTentativas == 3) {
 
-      // Medida preventiva limitando à apenas 3 tentativas caso a senha esteja errada
-      console.log("Quantidade de tentativas excedidas");
-      return true;
+        // Medida preventiva limitando à apenas 3 tentativas caso a senha esteja errada
+        console.log("Quantidade de tentativas excedidas");
+        return true;
+      }
     }
     return false;
   }
@@ -40,15 +70,14 @@ function ValidarSenha(senhaInput) {
 
 // Função responsável por realizar as tentativas de quebra de senha
 function ForcaBruta() {
-  
+
   // Declaração da variável para guardar o valor do momento de início da quebra da senha
   var inicioTempo = new Date().getTime();
 
-  for (let index = 0; index < 999999; index++) {
-    
+  for (let index = 1; index < 999999; index++) {
+
     // Contando a quantidade de tentativas realizadas
-    var tentativa = index + 1;
-    quantidadeDeTentativas = tentativa;
+    quantidadeDeTentativas++
     console.log(`Tentativa: ${quantidadeDeTentativas}`);
 
     let senhaForcaBruta = index.toString();
